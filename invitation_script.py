8fcae84105f4fd49a7351197d47bacec8f08579a
VISHAL108-Mech <vy4122000@gmail.com>
2026-09-18 14:22:55 +0530

    adding docstrings and comments

--- invitation_script.py
+++ invitation_script.py
@@ -1,11 +1,26 @@
-with open("Input/Names/invited_names.txt", "r") as f1:
+"""Script to automate the process of sending birthday invitations to a list of people."""
+
+sender_name = input("Enter Your Name: ")
+
+# Read the names of the invited people from a text file.
+with open("Location of invitation names file", "r") as f1:
     names = f1.readlines()
 
-with open("Input/Letters/starting_letter.txt", "r") as f2:
+# Read the invitation template from a text file.
+with open("Location of invitation template file", "r") as f2:
     content = f2.read()
 
+# Generate personalized invitations for each invited person.
 for name in names:
     name = name.strip()
-    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as f3:
-        replace_name = content.replace("[name]", name)
-        f3.write(replace_name)
+    invitation = content.replace("[Invited Person’s Name]", name)
+    invitation = invitation.replace("[Sender’s Name]", sender_name)
+    invitation = invitation.replace("[Insert Date]", "xx-xx-xxxx")
+    invitation = invitation.replace("[Insert Location]", "xxxxx")
+    invitation = invitation.replace("[Insert Time]", "xx:xx AM/PM")
+    invitation = invitation.replace("[RSVP Date]", "xx-xx-xxxx")
+
+    # Write the personalized invitation to a new text file for each invited person.
+    with open(
+        f"Location of invitation letters file/letter_for_{name}.txt","w+",) as f3:
+        f3.write(invitation)
